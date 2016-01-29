2c274a8f9c5240ceeed32657e13fb8cb5611efea
Paul Grochocinski <p.grochocinski@hotmail.ca>
2016-01-29 18:10:10 -0500

    Room placement and Shooting

---
+++ Symbiosis/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag != "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
