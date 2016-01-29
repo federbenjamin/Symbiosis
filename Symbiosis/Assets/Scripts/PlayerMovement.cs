f28f9f91e7049ef2227ba1dfce32a2458c684ad5
Paul Grochocinski <p.grochocinski@hotmail.ca>
2016-01-29 16:45:31 -0500

    Backwards game controls with script

---
+++ Symbiosis/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody playerRB;
+	private string playerPrefix;
+	private float horizMov, vertMov;
+	private Vector3 playerMov;
+	public float baseSpeed = 30f;
+
+
+	// Use this for initialization
+	void Start () {
+		playerRB = GetComponent<Rigidbody> ();
+		playerPrefix = gameObject.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+
+		//Player Movement
+		horizMov = Input.GetAxisRaw ("Horizontal" + playerPrefix);
+		vertMov = Input.GetAxisRaw ("Vertical" + playerPrefix);
+		playerMov = new Vector3 (horizMov, 0f, vertMov);
+		playerRB.AddForce (playerMov * (baseSpeed * 100) * Time.deltaTime);
+
+	}
+}
