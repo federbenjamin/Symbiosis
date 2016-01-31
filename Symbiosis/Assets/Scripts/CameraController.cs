5d473f52881ba0a4a697d92cc9c217c5e0ad3c58
Paul Grochocinski <p.grochocinski@hotmail.ca>
2016-01-31 01:22:54 -0500

    New Look for CameraP1, thoughts? Multiple room layout with working doors. Need work on making it nice but it is just a prototype.

---
+++ Symbiosis/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 newCameraPos;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("P1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		if (player.transform.position.x - transform.position.x >= 1) {
+			newCameraPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+			transform.position = Vector3.Lerp (transform.position, newCameraPos, 4.5f * Time.deltaTime);
+		} 
+		else if (player.transform.position.x - transform.position.x <= -1) {
+			newCameraPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+			transform.position = Vector3.Lerp (transform.position, newCameraPos, 4.5f * Time.deltaTime);
+		} 
+	}
+}
