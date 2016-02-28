a828d14bf01e2164ade53208ae3593e9043d72ca
Paul Grochocinski <p.grochocinski@hotmail.ca>
2016-02-28 13:54:47 -0500

    2 Switches that when pressed at once will open the doors in Room11

---
+++ Symbiosis/Assets/Scripts/SwitchesController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchesController : MonoBehaviour {
+
+	private RoomController roomController;
+
+	// Use this for initialization
+	void Start () {
+		roomController = transform.parent.GetComponent<RoomController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			roomController.switchesActive += 1;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			roomController.switchesActive -= 1;
+		}
+	}
+}
