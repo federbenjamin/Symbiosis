db12b41f4988854f42d53405c7211cafea4e602c
Benjamin Feder <canadianbif@users.noreply.github.com>
2016-03-03 00:37:43 -0500

    Changed Enemy2 AI shooting distance

--- Symbiosis/Assets/Scripts/Enemy2Behavior.cs
+++ Symbiosis/Assets/Scripts/Enemy2Behavior.cs
@@ -30,15 +30,15 @@ public class Enemy2Behavior : EnemyBehavior {
 		Vector3 moveDirection = myTransform.forward;
 		moveDirection.y = 0;
 		//move towards the player
-		if (targetDist > 7) {
+		if (targetDist > 6) {
 			myTransform.position += moveDirection * moveSpeed * Time.deltaTime;
-		} else if (targetDist < 5) {
+		} else if (targetDist < 3) {
 			if (collisionNormal.z == -1 || collisionNormal.z == 1) {
 				moveDirection.z = (-0.5f * collisionNormal.z);
 			} else if (collisionNormal.x == -1 || collisionNormal.x == 1) {
 				moveDirection.x = (-0.5f * collisionNormal.x);
 			}
-			myTransform.position -= moveDirection * (moveSpeed + 2) * Time.deltaTime;
+			myTransform.position -= moveDirection * (moveSpeed - 2) * Time.deltaTime;
 		} else if (Time.time > nextFire) {
 			Shoot(myTransform.forward);
 		}
