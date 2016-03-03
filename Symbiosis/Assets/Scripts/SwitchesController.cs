a34b9650cb7f9d6095d237e12bcd402deef27370
Paul Grochocinski <p.grochocinski@hotmail.ca>
2016-03-03 06:20:26 -0500

    Added new switches with movement when stepped on, New gameover and start screens

--- Symbiosis/Assets/Scripts/SwitchesController.cs
+++ Symbiosis/Assets/Scripts/SwitchesController.cs
@@ -4,26 +4,34 @@ using System.Collections;
 public class SwitchesController : MonoBehaviour {
 
 	private RoomController roomController;
+	private GameObject button;
 
 	// Use this for initialization
 	void Start () {
 		roomController = transform.parent.GetComponent<RoomController> ();
+		foreach (Transform child in transform) {
+			if (child.name == "Curve_001") {
+				button = child.gameObject;
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Player") {
 			roomController.switchesActive += 1;
+			button.transform.Translate(0,0,-0.1f);
 		}
 	}
 
 	void OnTriggerExit(Collider other) {
 		if (other.tag == "Player") {
 			roomController.switchesActive -= 1;
+			button.transform.Translate(0, 0,0.1f);
 		}
 	}
 }
