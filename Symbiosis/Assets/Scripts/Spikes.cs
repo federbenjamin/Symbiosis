d3ca15500e09bb92b21f35198684cbe584a6ac9b
Paul Grochocinski <p.grochocinski@hotmail.ca>
2016-03-22 07:11:23 -0400

    Made spikes work and damage the player

---
+++ Symbiosis/Assets/Scripts/Spikes.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour {
+
+	private HealthManager healthManager;
+	private Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+		healthManager = GameObject.FindWithTag("Health").GetComponent<HealthManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.tag == "Player") {
+			healthManager.DamageHealth(1);
+			dir = other.transform.position - transform.position;
+			other.GetComponent<Rigidbody>().AddForce(dir * 400);
+		}
+	}
+}
