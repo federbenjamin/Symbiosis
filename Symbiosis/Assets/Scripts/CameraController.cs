317d513bdc2129a7000d3294ff2ba9e611d5c9e5
Paul Grochocinski <p.grochocinski@hotmail.ca>
2016-03-24 00:49:56 -0400

    Premlim version of camera, needs tweaks but I think it works?

--- Symbiosis/Assets/Scripts/CameraController.cs
+++ Symbiosis/Assets/Scripts/CameraController.cs
@@ -14,10 +14,16 @@ public class CameraController : MonoBehaviour {
 
 	private Camera camera;
 
+	public GameObject cameraPointer;
+	private float deltaDist;
+	private bool facingRight;
+	float camMoveSpeed = 3f;
+
 	// Use this for initialization
 	void Start () {
 		camera = GameObject.Find ("CameraP2").GetComponent<Camera> ();
 		player2 = GameObject.Find ("P2");
+		newCameraPos = new Vector3 (player.transform.position.x, transform.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
@@ -25,7 +31,7 @@ public class CameraController : MonoBehaviour {
 	
 	}
 
-	void FixedUpdate () {
+	void LateUpdate () {
 
 		if (playersTogether == true) {
 			playersMiddle = (player.transform.position.x + player2.transform.position.x) * 0.5f;
@@ -34,16 +40,34 @@ public class CameraController : MonoBehaviour {
 				transform.position = Vector3.Lerp (transform.position, newCameraPos, 1f * Time.deltaTime);
 			}
 		} else {
-			if ((player.transform.position.x - transform.position.x >= 0.75f) || (player.transform.position.x - transform.position.x <= -0.75f)) {
-				float camMoveSpeed = 2f;
-
-				if ((player.transform.position.x - transform.position.x >= 1.25f) || (player.transform.position.x - transform.position.x <= -1.25f)) {
-					camMoveSpeed = 2.5f;
+			deltaDist = cameraPointer.transform.position.x - newCameraPos.x;
+			Debug.Log("DD " + deltaDist + " CP " + cameraPointer.transform.position.x);
+			if (facingRight == false) {
+				//Far Right
+				Debug.Log("ENTERED LEFT");
+				if (deltaDist > 4.5) {
+					Debug.Log("ENTERED >=2 L");
+					facingRight = true;
+					Debug.Log("BOOL " + facingRight);
+					newCameraPos = new Vector3 (player.transform.position.x + 2, transform.position.y, transform.position.z);
+				} else if (deltaDist <= - 0.5f) { 
+					Debug.Log("ENTERED <= 0.2 L");
+					newCameraPos = new Vector3 (cameraPointer.transform.position.x, transform.position.y, transform.position.z);
 				}
-
-				newCameraPos = new Vector3 (player.transform.position.x, transform.position.y, transform.position.z);
 				transform.position = Vector3.Lerp (transform.position, newCameraPos, camMoveSpeed * Time.deltaTime);
-			} 
+			} else if (facingRight == true) {
+				//Far Left
+				Debug.Log("ENTERED RIGHT");
+				if (deltaDist < -4.5) {
+					Debug.Log("ENTERED >=2 R");
+					facingRight = false;
+					newCameraPos = new Vector3 (player.transform.position.x - 2, transform.position.y, transform.position.z);
+				} else if (deltaDist >= 0.5f) {
+					Debug.Log("ENTERED <= 0.2 R");
+					newCameraPos = new Vector3 (cameraPointer.transform.position.x, transform.position.y, transform.position.z);
+				}
+				transform.position = Vector3.Lerp (transform.position, newCameraPos, camMoveSpeed * Time.deltaTime);
+			}
 		}
 	}
 
