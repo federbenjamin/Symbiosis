5978887229e1fa92ec13c413fd1af290104ea298
Benjamin Feder <canadianbif@users.noreply.github.com>
2016-03-31 07:10:11 -0400

    Boxes fade on destruction

--- Symbiosis/Assets/Scripts/ExplodingCrate.cs
+++ Symbiosis/Assets/Scripts/ExplodingCrate.cs
@@ -4,15 +4,45 @@ using System.Collections;
 public class ExplodingCrate : MonoBehaviour {
 	private Collider P1Collider;
 	private Collider P2Collider;
+	private Renderer[] rendererObjects;
+	private bool fadeOut;
+	private bool setFadeRendering;
+	private float alphaValue = 1f;
 
 	// Use this for initialization
 	void Start () {
-	
+		//rendererObjects = GetComponentsInChildren<Renderer>(); 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if (fadeOut) {
+			alphaValue = alphaValue - 0.02f;
+			Material material;
+			//foreach (Renderer child in rendererObjects) {
+			foreach (Transform child in transform) {
+				material = child.GetComponent<Renderer>().material;
+				if (!setFadeRendering) {
+					material.SetFloat("_Mode", 2);
+					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+					material.SetInt("_ZWrite", 0);
+					material.DisableKeyword("_ALPHATEST_ON");
+					material.EnableKeyword("_ALPHABLEND_ON");
+					material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+					material.renderQueue = 3000;
+	            }
+	            
+				Color newColor = material.color;
+				newColor.a = alphaValue;
+				Debug.Log(newColor.a);
+				material.SetColor("_Color", newColor);
+			}
+			setFadeRendering = true;
+		}
+		if (alphaValue <= 0) {
+			Destroy(this.gameObject);
+		}
 	}
 
 	void OnTriggerEnter (Collider other) {
@@ -27,6 +57,13 @@ public class ExplodingCrate : MonoBehaviour {
 				Physics.IgnoreCollision(P1Collider, child.GetComponent<Collider>());
 				Physics.IgnoreCollision(P2Collider, child.GetComponent<Collider>());
 			}
+
+			StartCoroutine("FadeOut");
 		}
+	}
+
+	IEnumerator FadeOut() {
+		yield return new WaitForSeconds (1f);
+		fadeOut = true;
 	}
 }
