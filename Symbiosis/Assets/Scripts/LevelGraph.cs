6ac3fa0f27635ab51b826f8983d849ee081e2a10
Benjamin Feder <canadianbif@users.noreply.github.com>
2016-04-02 18:18:50 -0400

    Added initial level graph classes

---
+++ Symbiosis/Assets/Scripts/LevelGraph.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGraph {
+
+	public string player;
+	public Node tutorialAdjRoom;
+	public Node switchAdjRoom;
+	public Node[] roomList;
+	public Vertex[] doorList;
+
+}
+
+public class Node {
+
+	public int roomNumber;
+	public int difficulty;
+	// public string color;
+
+}
+
+public class Vertex {
+
+	public Door door1;
+	public Door door2;
+
+}
+
+public class Door {
+
+	public int roomNumber;
+	public string direction;
+
+}
