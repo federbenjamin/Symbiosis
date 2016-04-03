936dac7c065be823526f3efb406f6d7279d97e9f
Benjamin Feder <canadianbif@users.noreply.github.com>
2016-04-02 21:27:10 -0400

    Added full graph representation of level

--- Symbiosis/Assets/Scripts/LevelGraph.cs
+++ Symbiosis/Assets/Scripts/LevelGraph.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 
 public class LevelGraph {
 
-	public string player;
+	public string player; // ???
 
-	public Node tutorialAdjRoom;
-	private Node TutorialAdjRoom {
+	private Node tutorialAdjRoom;
+	public Node TutorialAdjRoom {
 		get{return tutorialAdjRoom;}
 		set{tutorialAdjRoom = value;}
 	}
@@ -21,23 +21,48 @@ public class LevelGraph {
 	public List<Node> RoomList {
 		get{return roomList;}
 	}
-	private List<Vertex> doorList;
-	public List<Vertex> DoorList {
+	private List<Edge> doorList;
+	public List<Edge> DoorList {
 		get{return doorList;}
 	}
 
 	public LevelGraph(string player) {
     	this.player = player;
     	roomList = new List<Node>();
-    	doorList = new List<Vertex>();
+    	doorList = new List<Edge>();
     }
 
     public void AddRoom(Node newRoom) {
     	roomList.Add(newRoom);
     }
 
-    public void AddDoor(Vertex newDoor) {
+    public void AddDoor(Edge newDoor) {
     	doorList.Add(newDoor);
+    }
+
+    public void Print() {
+    	Debug.Log(player + ": ");
+    	Debug.Log("Tutorial Room: " + tutorialAdjRoom.roomNumber);
+    	PrintEdges(tutorialAdjRoom);
+    	Debug.Log("Switch Room: " + switchAdjRoom.roomNumber);
+    	PrintEdges(switchAdjRoom);
+    	foreach (Node coreRoom in roomList) {
+    		Debug.Log("Room: " + coreRoom.roomNumber);
+    		PrintEdges(coreRoom);
+    	}
+    }
+
+    public void PrintEdges(Node node) {
+    	int roomNumber = node.roomNumber;
+    	foreach (Edge edge in doorList) {
+    		int door1Num = edge.door1.RoomNumber;
+    		int door2Num = edge.door2.RoomNumber;
+    		if (door1Num == roomNumber) {
+    			Debug.Log("   Door: " + door1Num + " to " + door2Num);
+    		} else if (door2Num == roomNumber) {
+    			Debug.Log("   Door: " + door2Num + " to " + door1Num);
+    		}
+    	}
     }
 
 }
@@ -46,25 +71,47 @@ public class Node {
 
 	public int roomNumber;
 	public int difficulty;
-	// public string color;
+
+	public enum Color {White, Grey, Black}
+	public Color color;
 
 	public Node(int roomNumber) {
     	this.roomNumber = roomNumber;
+    	this.color = Color.White;
     }
 
 }
 
-public class Vertex {
+public class Edge {
 
 	public Door door1;
 	public Door door2;
 
-	public Vertex(Door door1, Door door2) {
+	public Edge(Door door1, Door door2) {
     	this.door1 = door1;
     	this.door2 = door2;
     }
 
-    public Vertex(int roomNumber1, string direction1, int roomNumber2, string direction2) {
+	public Edge(int roomNumber1, string direction1, int roomNumber2) {
+    	Door door1 = new Door(roomNumber1, direction1);
+
+    	string direction2 = "";
+    	if (direction1 == "East") {
+			direction2 = "West";
+		} else if (direction1 == "West") {
+			direction2 = "East";
+		} else if (direction1 == "North") {
+			direction2 = "South";
+		} else {
+			direction2 = "North";
+		}
+    	Door door2 = new Door(roomNumber2, direction2);
+
+    	this.door1 = door1;
+    	this.door2 = door2;
+    }
+
+    public Edge(int roomNumber1, string direction1, int roomNumber2, string direction2) {
     	Door door1 = new Door(roomNumber1, direction1);
     	Door door2 = new Door(roomNumber2, direction2);
     	this.door1 = door1;
